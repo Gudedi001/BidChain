95ac138f1e307fdefae1900e936777a33c9672b8
Gideon <g16621000646@163.com>
2025-04-08 09:46:49 +0800

    nothing

--- test/TestChain.js
+++ test/TestChain.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat");
 
 async function main() {
   // 创建 JsonRpcProvider 连接 Arbitrum Sepolia
-  const provider = new ethers.providers.JsonRpcProvider("https://arb-sepolia.g.alchemy.com/v2/6Bf3RM8Z7ZgQWxqox-QTqsy5fjtI2yf2");
+  const provider = new ethers.providers.JsonRpcProvider("https://arb-sepolia.g.alchemy.com/v2/"+APIKEY);
 
   // 获取最新区块
   const block = await provider.getBlock("latest");
