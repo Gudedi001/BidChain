ea28399a8d6060e6ff52c6aa377af9bcf4551e0f
Gideon <g16621000646@163.com>
2025-04-24 21:26:44 +0800

    deploy operation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -18,10 +18,10 @@ const { ethers, upgrades } = require("hardhat")
     Block Scan address:https://sepolia.arbiscan.io/
  * assetStorage contract deployed to: 0x66d0Ff0405fff81508095cE1BCaf9B3A7D437526       
  * nftAuction contract deployed to: 0x2Ac3d1FED9531A7d384eD41bb1bd4a12563c1f18          代理地址
-     nftAuction ImplementationAddress: 0xd271D45A50796D9b7b71808888E8B988534D3FA1
+     nftAuction ImplementationAddress: 0x724A68B5965CdC2098B07f7BDdF7543E739E6253
      nftAuction AdminAddress: 0x86062B455CAf4D47d3f0393DD9336B5e8FE57a7E
-   auction contract deployed to: 0xe3360c5Ee0f097633FCE8fAE1f2F923F483ddAEA             代理地址
-      auction ImplementationAddress: 0xfC5E2DE38f9f5e0A5b8992C5093b0C6F56Dd7220
+   auction contract deployed to: 0x1B99C667C9C73b0477a9453694D66775032fd4E3             代理地址
+      auction ImplementationAddress: 0xb7f616535AE4946aFeFf53b0D3000963FE8bfCF9
       auction AdminAddress: 0x86062B455CAf4D47d3f0393DD9336B5e8FE57a7E
 
 
@@ -44,20 +44,20 @@ async function main() {
     // console.log("AssetStorage contract deployed to:", assetStorage.address);
 
     //deploy NFTAuction
-    let nftAuction = await ethers.getContractFactory("NFTAuction")
-    nftAuction = await upgrades.deployProxy(nftAuction,[5,"BIDC","BIDCSYMPOL"], { initializer: 'initialize' });
-    await nftAuction.deployed()
-    console.log("NFTAuction contract deployed to:", nftAuction.address);
-    console.log(await upgrades.erc1967.getImplementationAddress(nftAuction.address), " nftAuction getImplementationAddress");
+    // let nftAuction = await ethers.getContractFactory("NFTAuction")
+    // nftAuction = await upgrades.deployProxy(nftAuction,[5,"BIDC","BIDCSYMPOL"], { initializer: 'initialize' });
+    // await nftAuction.deployed()
+    // console.log("NFTAuction contract deployed to:", nftAuction.address);
+    // console.log(await upgrades.erc1967.getImplementationAddress(nftAuction.address), " nftAuction getImplementationAddress");
 
     //deploy Auction
-    // let nftAddress="0x82C38fF1ca69Cf0f9589dc05D2B76049034B7C95";
-    // let assetAddress="0x6a18C66B385b6F33D3090308a926c83582C4FAEA";
-    // let auction = await ethers.getContractFactory("Auction");
-    // auction = await upgrades.deployProxy(auction,[assetAddress,nftAddress], { initializer: 'initialize' });
-    // await auction.deployed()
-    // console.log("Auction contract deployed to:", auction.address);
-    // console.log(await upgrades.erc1967.getImplementationAddress(auction.address), " auction getImplementationAddress");
+    let nftAddress="0x82C38fF1ca69Cf0f9589dc05D2B76049034B7C95";
+    let assetAddress="0x6a18C66B385b6F33D3090308a926c83582C4FAEA";
+    let auction = await ethers.getContractFactory("Auction");
+    auction = await upgrades.deployProxy(auction,[assetAddress,nftAddress], { initializer: 'initialize' });
+    await auction.deployed()
+    console.log("Auction contract deployed to:", auction.address);
+    console.log(await upgrades.erc1967.getImplementationAddress(auction.address), " auction getImplementationAddress");
 
   // https://sepolia.arbiscan.io/  arbitrum 区块浏览地址
   
--- scripts/upgrade-deploy.js
+++ scripts/upgrade-deploy.js
@@ -1,5 +1,8 @@
 const { ethers, upgrades } = require("hardhat");
-
+/**
+ *  npx hardhat run scripts/upgrade-deploy.js --network sepolia
+ *  New NFTAuctionV2 implementation deployed to: 0x724A68B5965CdC2098B07f7BDdF7543E739E6253
+ */
 async function main() {
   // 代理合约地址（从之前的部署日志获取）
   const nftAuctionProxyAddress = "0x2Ac3d1FED9531A7d384eD41bb1bd4a12563c1f18"; 
@@ -9,16 +12,16 @@ async function main() {
 //   const auctionProxyAddress = "0xe3360c5Ee0f097633FCE8fAE1f2F923F483ddAEA";
 
   // 获取新版本的NFTAuction合约
-  const NFTAuctionV2 = await ethers.getContractFactory("NFTAuction");
-  console.log("Upgrading NFTAuction to V2...");
+//   const NFTAuctionV2 = await ethers.getContractFactory("NFTAuction");
+//   console.log("Upgrading NFTAuction to V2...");
   // 执行升级
-  const upgradedNftAuction = await upgrades.upgradeProxy(nftAuctionProxyAddress, NFTAuctionV2);
+//   const upgradedNftAuction = await upgrades.upgradeProxy(nftAuctionProxyAddress, NFTAuctionV2);
   // 等待升级完成
-  await upgradedNftAuction.deployed();
+//   await upgradedNftAuction.deployed();
   // 获取新实现合约的地址
-  const implementationAddressNftAuction = await upgrades.erc1967.getImplementationAddress(nftAuctionProxyAddress);
-  console.log("NFTAuction proxy upgraded at:", nftAuctionProxyAddress);
-  console.log("New implementation deployed to:", implementationAddressNftAuction);
+//   const implementationAddressNftAuction = await upgrades.erc1967.getImplementationAddress(nftAuctionProxyAddress);
+//   console.log("NFTAuction proxy upgraded at:", nftAuctionProxyAddress);
+//   console.log("New implementation deployed to:", implementationAddressNftAuction);
 
 
   // 获取新版本的Auction合约
@@ -30,8 +33,8 @@ async function main() {
   await upgradedAuction.deployed();
   // 获取新实现合约的地址
   const implementationAddressAuction = await upgrades.erc1967.getImplementationAddress(auctionProxyAddress);
-  console.log("NFTAuction proxy upgraded at:", auctionProxyAddress);
-  console.log("New implementation deployed to:", implementationAddressAuction);
+  console.log("Auction proxy upgraded at:", auctionProxyAddress);
+  console.log("New Auction implementation deployed to:", implementationAddressAuction);
 
 }
 
